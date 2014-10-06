511004c0228f3d1fecd9ee78301f0d09115c3579
yvettecook <yvettecook@gmail.com>
2014-10-06 14:19:52 +0100

    Development and test databases added

--- lib/router.rb
+++ lib/router.rb
@@ -1,4 +1,15 @@
 require 'sinatra/base'
+require 'data_mapper'
+
+env = ENV["RACK_ENV"] || "development"
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link'
+
+DataMapper.finalize
+
+DataMapper.auto_upgrade! 
 
 class BookmarkManager < Sinatra::Base
   get '/' do
