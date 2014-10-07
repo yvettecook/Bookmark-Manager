7ae0d7a787387da0e876e90e7e2981b96a54d248
yvettecook <yvettecook@gmail.com>
2014-10-07 11:33:16 +0100

    User can filter links by tag

--- lib/router.rb
+++ lib/router.rb
@@ -29,6 +29,12 @@ set :views, Proc.new { File.join(root, "..", "views")}
   	redirect to('/')
   end
 
+  get '/tags/:text' do
+    tag = Tag.first(:text => params[:text])
+    @links = tag ? tag.links : []
+    erb :index
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
---
+++ spec/features/filtering_by_tags_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+feature "User can filter links by tag" do 
+
+	before(:each) do
+		Link.create(:url => "http://www.makersacademy.com",
+                :title => "Makers Academy",
+                :tags => [Tag.first_or_create(:text => 'education')])
+    Link.create(:url => "http://www.google.com",
+                :title => "Google",
+                :tags => [Tag.first_or_create(:text => 'search')])
+    Link.create(:url => "http://www.bing.com",
+                :title => "Bing",
+                :tags => [Tag.first_or_create(:text => 'search')])
+    Link.create(:url => "http://www.code.org",
+                :title => "Code.org",
+                :tags => [Tag.first_or_create(:text => 'education')])
+  end
+
+	scenario "filtering by a tag" do
+		visit '/tags/search'
+		expect(page).not_to have_content("Makers Academy")
+		expect(page).not_to have_content("Code.org")
+		expect(page).to have_content("Google")
+		expect(page).to have_content("Bing")
+		end
+
+
+end
