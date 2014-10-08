b919644a06d58dd2e9b92ade786ea1f70d1b41d7
yvettecook <yvettecook@gmail.com>
2014-10-08 14:25:46 +0100

    user cannot sign in with incorrect credentials

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -15,11 +15,19 @@ feature "User signs in" do
 		expect(page).to have_content("Welcome, test@test.com")
 	end
 
+	scenario "with incorrect credentials" do
+		visit '/'
+		expect(page).not_to have_content("Welcome, test@test.com")
+		sign_in('test@test.com', 'wrong')
+		expect(page).not_to have_content("Welcome, test@test.com")
+	end
+
 	def sign_in(email, password)
 		visit '/sessions/new'
 		fill_in 'email', :with => email
 		fill_in 'password', :with => password
 		click_button 'Sign in'
 	end
+ 
 
 end
