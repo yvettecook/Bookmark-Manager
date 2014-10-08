b37e97869b3ca47232db4fe8c54aeb6b491ff14e
yvettecook <yvettecook@gmail.com>
2014-10-08 11:11:10 +0100

    Password validation

--- app/router.rb
+++ app/router.rb
@@ -42,10 +42,12 @@ class BookmarkManager < Sinatra::Base
 
     post '/users' do
       user = User.create(:email => params[:email],
-                  :password => params[:password])
+                  :password => params[:password],
+                  :password_confirmation => params[:password_confirmation])
       session[:user_id] = user.id
       redirect to('/')
     end
+
 
     # start the server if ruby file executed directly
     run! if app_file == $0
--- lib/user.rb
+++ lib/user.rb
@@ -4,12 +4,18 @@ class User
 
 	include DataMapper::Resource
 
-	property :id,			Serial
-	property :email,	String
+	property :id,											Serial
+	property :email,									String
+	property :password_digest, 				Text
+	property :password_confirmation,	Text
 
-	property :password_digest, Text
+	attr_reader :password
+	attr_accessor :password_confirmation
+
+	validates_confirmation_of :password
 
 	def password=(password)
+		@password = password
 		self.password_digest = BCrypt::Password.create(password)
 	end
 
