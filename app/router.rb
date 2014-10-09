8e183efba5a699f3e10264427a1d7965d68625db
yvettecook <yvettecook@gmail.com>
2014-10-09 16:57:12 +0100

    Implemented sinatra partials in views directory

--- app/router.rb
+++ app/router.rb
@@ -1,26 +1,26 @@
 require 'sinatra'
 require 'data_mapper'
 require 'rack-flash'
+require 'sinatra/partial'
 require './lib/link'
 require './lib/user'
 require './lib/tag'
+
 require_relative 'helpers/helpers'
 require_relative 'data_mapper_setup'
-
 require_relative 'controllers/application'
 require_relative 'controllers/links'
 require_relative 'controllers/tag'
 require_relative 'controllers/users'
 require_relative 'controllers/sessions'
 
+set :views, Proc.new { File.join(root, "..", "app", "views")}
+enable :sessions
+use Rack::Flash
+set :session_secret, 'super secret'
 
-  set :views, Proc.new { File.join(root, "..", "app", "views")}
-  enable :sessions
+set :partial_template_engine, :erb
 
-  use Rack::Flash
-  use Rack::MethodOverride
-
-  set :session_secret, 'super secret'
   
 
     
